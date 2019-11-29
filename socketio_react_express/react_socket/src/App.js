b2d47ab2f550b68317687efed81ac3079eab765d
Oscar Hermawan <Oscar_H261@mitrais.com>
2019-11-29 16:04:00 +0800

    sukses_socket_with_manual_chat

--- socketio_react_express/react_socket/src/App.js
+++ socketio_react_express/react_socket/src/App.js
@@ -5,32 +5,45 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      chat:[]
+      chat:[],
+      pesan:"",
+      socket: io('localhost:3001')
     }
-    this.socket = io('localhost:3001');
-    let app = this
-    this.socket.on('message', function(data){
-        app.addMessage(data);
-    });
-
-    
-    this.addMessage = data => {
-      this.setState({chat: [...this.state.chat, data]});
-    };
   }
 
+  componentDidMount(){ 
+    let app = this
+    this.state.socket.on('message', function(data){
+      app.getMessages(data)
+    })
+  }
+  
+  getMessages(data){
+    this.setState({chat: [...this.state.chat, data]});
+  }
 
-
-  testing = (value) => {
-    console.log('masuk emit')
+  sendMessages = (value) => {
+    console.log('masuk emit', value)
     const socket = io('localhost:3001')
     socket.emit('message', value)
   }
 
+  onFormSubmit(){
+    console.log('masuk submit')
+    this.sendMessages(this.state.pesan)
+  }
+
   render = () => 
-    <div className="App">
-      <button onClick={() => this.testing('woe')}>test</button>
-    </div>
+  <div className="App">
+    <ul>
+      {this.state.chat.map((cha,index) => <li key={index}>{cha}</li>)}
+    </ul>
+    {/* <form onSubmit={() => this.onFormSubmit()}> */}
+      <input type="text" value={this.state.pesan} onChange={(e) => this.setState({pesan:e.target.value})}/>
+      <button onClick={() => this.sendMessages(this.state.pesan)}>submit</button>
+    {/* </form> */}
+    
+  </div>
 }
 
 export default App;
